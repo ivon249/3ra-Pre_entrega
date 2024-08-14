2974ee1b43330c012744d04bb533e7479ff228f1
ivon249 <ivon249@hotmail.com>
2024-08-14 09:11:14 -0300

    Se agrega el proyecto

---
+++ pruebaconst.js
@@ -0,0 +1,47 @@
+function Sendero(id,nombre,dificultad,tiempoenhs,distanciaenkm) {
+    this.id = id;
+    this.nombre = nombre;
+    this.dificultad = dificultad;
+    this.tiempoenhs = tiempoenhs;
+    this.distanciaenkm = distanciaenkm
+}
+const Sendero1 = new Sendero (1,"Sendero Laguna Sofía","Baja",6,2);
+const Sendero2 = new Sendero (2,"Sendero Mirador Cabo San Pablo","Baja",4,4,2);
+const Sendero3 = new Sendero (3,"Sendero de la Ruca","Baja",2,4,0,5);
+const Sendero4 = new Sendero (4,"Sendero Laguna Negra","Baja",4,2);
+const Sendero5 = new Sendero (5,"Sendero al Lago Fagnano","Media",12,4);
+
+console.log("Sendero1")
+console.log(Sendero1.id);
+console.log(Sendero1.nombre);
+console.log(Sendero1.dificultad);
+console.log(Sendero1.tiempoenhs);
+console.log(Sendero1.distanciaenkm);
+
+console.log("Sendero2")
+console.log(Sendero2.id);
+console.log(Sendero2.nombre);
+console.log(Sendero2.dificultad);
+console.log(Sendero2.tiempoenhs);
+console.log(Sendero2.distanciaenkm);
+
+console.log("Sendero3")
+console.log(Sendero3.id);
+console.log(Sendero3.nombre);
+console.log(Sendero3.dificultad);
+console.log(Sendero3.tiempoenhs);
+console.log(Sendero3.distanciaenkm);
+
+console.log("Sendero4")
+console.log(Sendero4.id);
+console.log(Sendero4.nombre);
+console.log(Sendero4.dificultad);
+console.log(Sendero4.tiempoenhs);
+console.log(Sendero4.distanciaenkm);
+
+console.log("Sendero5")
+console.log(Sendero5.id);
+console.log(Sendero5.nombre);
+console.log(Sendero5.dificultad);
+console.log(Sendero5.tiempoenhs);
+console.log(Sendero5.distanciaenkm);
